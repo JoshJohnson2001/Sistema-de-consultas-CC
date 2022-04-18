fee43486f9f0e85cf47db047edcccb7bb40a4e95
JoshJohnson2001 <eduardojohnson2001@gmail.com>
2022-04-18 03:47:06 -0600

    Supplier

--- src/controllers/supplierController.js
+++ src/controllers/supplierController.js
@@ -30,7 +30,17 @@ controller.save = (req, res) => {
 
 
 controller.create = (req, res) => {
-  res.render('Supplier_view/Supplier_Create');
+  req.getConnection((err, conn) => {
+    conn.query('SELECT * FROM Supplier', (err, listaA) => {
+      if (err) {
+        res.json(err);
+      }
+      else {
+        res.render('Supplier_view/Supplier_Create');
+      }
+    });
+  });
+  
 };
 
 
@@ -60,6 +70,88 @@ controller.delete = (req, res) => {
   req.getConnection((err, connection) => {
     connection.query('DELETE FROM Supplier WHERE supplier_id = ?', [id], (err, rows) => {
       res.redirect('/supplier');
+    });
+  });
+}
+
+//Product for Supplier
+
+controller.listProduct = (req, res) => {
+  const { id } = req.params;
+  req.getConnection((err, conn) => {
+        conn.query('SELECT p.product_name,p.price,p.category_name,p.subcategory_name,p.is_available from SupplierStock as ss INNER JOIN Product as p on p.product_id = ss.product_id WHERE ss.supplier_id = ?', [id], (err, listaA) => {
+      if (err) {
+        res.json(err);
+      }
+      else {
+        res.render('Supplier_view/Supplier_Product_List', {
+          dataID: id,
+          data: listaA
+        });
+      }
+    });
+  });
+};
+
+controller.saveProduct = (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  console.log(req.body)
+  req.getConnection((err, connection) => {
+    const query = connection.query('INSERT INTO Product set ?', data, (err, Supplier) => {})
+    query = connection.query('SELECT product_id FROM Product WHERE product_name = ?',data.product_name,(err, product) => {
+      const query2 = connection.query('INSERT INTO SupplierStock (supplier_id, product_id) VALUES (?, ?);',[id] ,product, (err, Supplier) => {})
+      res.redirect('/supplier/<%=id%>/product');
+    })
+    
+  })
+};
+
+
+controller.createProduct = (req, res) => {
+  const { id } = req.params;
+  req.getConnection((err, conn) => {
+    conn.query('SELECT * FROM Supplier', (err, listaA) => {
+      if (err) {
+        res.json(err);
+      }
+      else {
+        res.render('Supplier_view/Product_Create',{
+          data: id
+        });
+      }
+    });
+  });
+  
+};
+
+
+controller.editProduct = (req, res) => {
+  const { id } = req.params;
+  req.getConnection((err, conn) => {
+    conn.query("SELECT * FROM Supplier WHERE supplier_id = ?", [id], (err, rows) => {
+      res.render('Supplier_view/Supplier_Edit', {
+        data: rows[0]
+      })
+    });
+  });
+};
+
+controller.updateProduct = (req, res) => {
+  const { id } = req.params;
+  const newSupplier = req.body;
+  req.getConnection((err, conn) => {
+    conn.query('UPDATE Supplier set ? where supplier_id = ?', [newSupplier, id], (err, rows) => {
+      res.redirect('/supplier/product');
+    });
+  });
+};
+
+controller.deleteProduct = (req, res) => {
+  const { id } = req.params;
+  req.getConnection((err, connection) => {
+    connection.query('DELETE FROM Supplier WHERE supplier_id = ?', [id], (err, rows) => {
+      res.redirect('/supplier/product');
     });
   });
 }
--- src/routes/supplier.js
+++ src/routes/supplier.js
@@ -3,9 +3,16 @@ const router = express.Router();
 const controller = require('../controllers/supplierController');
 
 router.get('/supplier', controller.list);
-router.get('/supplier')
+router.get('/supplier/create',controller.create);
 router.post('/supplier/add', controller.save);
 router.get('/supplier/edit/:id', controller.edit);
 router.post('/supplier/update/:id', controller.update);
 router.get('/supplier/delete/:id', controller.delete);
+
+router.get('/supplier/:id/product', controller.listProduct);
+router.get('/supplier/:id/product/create',controller.createProduct);
+router.post('/supplier/:id/product/add', controller.saveProduct);
+router.get('/supplier/:id/product/edit/:id', controller.editProduct);
+router.post('/supplier/:id/product/update/:id', controller.updateProduct);
+router.get('/supplier/:id/product/delete/:id', controller.deleteProduct);
 module.exports = router;
